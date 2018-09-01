e0855d0c892a9756576e6f9aa37b0113ee991d79
Thaddeus M. Diede <thaddeusmd@gmail.com>
2018-09-01 14:40:23 -0600

    add - first submission - git client initialization

---
+++ git/git-client-setup.sh
@@ -0,0 +1,19 @@
+# git linux client configuration for TMD
+
+# 20180901 - Pulled from wikipad documentation
+
+
+git config --global user.name "Thaddeus M. Diede"
+git config --global user.email "thaddeusmd@gmail.com"
+git config --global core.editor "vim"
+git config --global color.ui auto
+git config --global merge.conflictstyle diff3
+
+# enable credential storage
+git config --global credential.helper cache
+
+# retain credentials for 2 hours (in seconds)
+git config --global credential.helper 'cache --timeout=7200'
+
+# display current paramaters
+git config --list
